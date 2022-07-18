b543642866c036efa55a1a9ccd0fe250b94092be
seanpeng12 <seanpeng12@gmail.com>
2022-07-18 17:11:10 +0800

    quickSort_DS完成

--- myTestCode/quickSort.py
+++ myTestCode/quickSort.py
@@ -7,53 +7,38 @@ def QS_DSver_partition(arr,i,j,pk):
     while(True):
         while(True):
             if(arr[i] >= arr[pk]):
-                print("i found:%d" % arr[i])
-                for data in arr: print(data, end=" ")
-                print("\n")
+                # print("i found:%d" % arr[i])
+                # for data in arr: print(data, end=" ")
+                # print("\n")
                 break
             i+=1
         while(True):
             if(arr[j] <= arr[pk]):
-                print("j found:%d" % arr[j])
-                for data in arr: print(data, end=" ")
-                print("\n")
+                # print("j found:%d" % arr[j])
+                # for data in arr: print(data, end=" ")
+                # print("\n")
                 break
             j-=1 
   
         if(i > j):
             SWAP(arr,pk,j) # pk SWAP with j, j be a new pk
-            for data in arr: print(data, end=" ")
-            print("\n")
-            # return origin_i,origin_j, j
+            # print("pk to j!")
+            # for data in arr: print(data, end=" ")
+            # print("\n=============\n")
             return j
         else:
             SWAP(arr,i,j)
-            for data in arr: print(data, end=" ")
-            print("\n")
-
+            # print("swap!")
+            # for data in arr: print(data, end=" ")
+            # print("\n")
             i+=1
             j-=1
         
 def QuickSort_DS(arr,i,j,pk):
-    # print()
     if(i < j):
         pk = QS_DSver_partition(arr,i,j,pk) #get index 5
-        print(pk)
-        # origin_i = res[0]
-        # origin_j = res[1]
-        # new_pk = res[2]
-
-        QuickSort_DS(arr,i,pk-1,arr[i])
-        QuickSort_DS(arr,pk+1,j,arr[pk+1])
-
-
-        # res1 = QS_DSver_partition(arr,0,4,0)    6 9 6
-        # print(res1)
-        # res2 = QS_DSver_partition(arr,0,1,0) 
-        # print(res2)
-
-    
-
+        QuickSort_DS(arr,i,pk-1,i)
+        QuickSort_DS(arr,pk+1,j,pk+1)
   
     
 def SWAP(arr,i,j):
@@ -63,4 +48,5 @@ def SWAP(arr,i,j):
     
 if __name__ == "__main__":
     arr = [26,5,37,1,61,11,59,15,48,19] # 10 items
-    res = QuickSort_DS(arr,0,9,0)
+    QuickSort_DS(arr,0,len(arr)-1,0)
+    for n in arr: print(n, end=" ") #result
